6fed139ab31a75840bc20256dba8cdc890e8c1d7
Victor Alvarenga <victor.alvar@tutanota.com>
2023-05-16 15:29:30 -0300

    Add fix test version.

--- src/modules/advertising/adsFeatures/AdsFeaturesElements.jsx
+++ src/modules/advertising/adsFeatures/AdsFeaturesElements.jsx
@@ -27,6 +27,27 @@ export const Wrapper = styled.div`
     height: auto;
   }
 `
+export const ListWrap = styled.div`
+  position: relative;
+  width: max-content;
+  padding-top: 4em;
+  padding-bottom: 4em;
+  @media screen and (max-width: 510px) {
+    width: 100%;
+  }
+
+  span {
+    font-size: 1.1em;
+    font-weight: 600;
+    color: #505a63;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    font-size: 1.9em;
+    padding-top: 0.31em;
+  }
+`
 
 export const ImageWrap = styled.div`
   display: flex;
@@ -60,6 +81,8 @@ export const StyledList = styled.ul`
     font-family: var(--font-heading);
     font-size: 1.4em;
     font-weight: 500;
+    /* flex-wrap: wrap; */
+    /* padding-right: 2em; */
   }
 `
 
