1de29854dbf7899ec52b4ecc666c4a401f9120d7
Victor Alvarenga <victor.alvar@tutanota.com>
2023-06-02 15:58:01 -0300

    Add update imageBanner componet in home page, improve responsiveness.

--- src/modules/home/imageBanner/ImageBannerElements.jsx
+++ src/modules/home/imageBanner/ImageBannerElements.jsx
@@ -24,23 +24,20 @@ export const Container = styled.div`
 
 export const ImageWrap = styled.div`
   position: fixed;
-  display: block;
   top: 0;
-  bottom: 0;
   left: 0;
-  right: 0;
   width: 100%;
   height: 100%;
 
   img {
-    position: absolute;
     width: 100%;
     height: 100%;
-    object-fit: cover;
     top: 0;
-    bottom: 0;
     left: 0;
-    right: 0;
+    object-fit: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: fixed;
     filter: brightness(80%);
   }
 `
