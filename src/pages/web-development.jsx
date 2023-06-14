2bc1c90e06881012d5d6e3a690a6a4792d9b9250
Victor Alvarenga <victor.alvar@tutanota.com>
2023-06-14 12:14:02 -0300

    Add fix deployment errors.

--- src/pages/web-development.jsx
+++ src/pages/web-development.jsx
@@ -1,5 +1,4 @@
 import {NextSeo} from 'next-seo'
-import Head from 'next/head'
 
 import CallToAction from '@/modules/web-development/callToAction/CallToAction'
 import DevCases from '@/modules/web-development/devCases/DevCases'
